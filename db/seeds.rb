85d27249f10fc62615b876cd9f39e2449cee1770
Matt Eves <mattheweves@outlook.com>
2017-04-20 20:44:57 -0600

    Null Checks & Logo Change

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Quote.create(saying: 'Ten percent of the big fish still remain. There are still some blue whales. There are still some krill in Antarctica. There are a few oysters in Chesapeake Bay. Half the coral reefs are still in pretty good shape, a jeweled belt around the middle of the planet. There is still time, but not a lot, to turn things around.', author: 'Sylvia Earle');
+Quote.create(saying: 'If you really think that the environment is less important than the economy, try holding your breath while you count your money.', author: 'Dr. Guy McPherson')
